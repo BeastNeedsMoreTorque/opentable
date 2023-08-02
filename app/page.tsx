374d620eb6b02ed15288129d27739760a1e43773
BeastNeedsMoreTorque <theironhammer1964@gmail.com>
2023-08-02 11:43:04 -0700

    fixed error in app/page.tsx. changed to client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation'; //'next/router';
 
 export default function Home() {
   const router = useRouter()
